c1fd536634156c0929e903f334a619ba18119e2e
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-04 00:19:45 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,9 +3,9 @@ require 'pry'
 
 
 def create_project_hash
-
+  
   kickstarter = Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
-  # kickstarter.css(".project grid_4").first
+  # projects: kickstarter.css(".project grid_4").first
 
   #____
   #return projects ??
